be7ba10b315d947a1a53cb62254c80be5fd8d24a
Yurii Gromyk <juriygromyk@gmail.com>
2019-06-26 11:35:22 +0200

    Add youtube button to TrackAdapter.

--- app/src/main/java/com/gromyk/lastfmaf/presentation/songs/TrackAdapter.kt
+++ app/src/main/java/com/gromyk/lastfmaf/presentation/songs/TrackAdapter.kt
@@ -9,9 +9,10 @@ import com.gromyk.lastfmaf.presentation.base.BaseRecyclerAdapter
 import com.gromyk.persistence.track.Track
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.song_item.*
+import java.lang.ref.WeakReference
 
-class TrackAdapter : BaseRecyclerAdapter<Track>() {
-
+class TrackAdapter(listener: OnYouTubeClicked) : BaseRecyclerAdapter<Track>() {
+    val onYouTubeClickListener = WeakReference(listener)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.song_item, parent, false))
 
@@ -19,15 +20,27 @@ class TrackAdapter : BaseRecyclerAdapter<Track>() {
         BaseRecyclerAdapter.ViewHolder<Track>(containerView!!),
         View.OnClickListener,
         LayoutContainer {
+
         override fun bindView(item: Track) {
             itemView.setOnClickListener(this)
             songNumberTextView.text = (adapterPosition + 1).toString()
             songNameTextView.text = item.name
             songArtistTextView.text = item.artistName ?: ""
             songTimeTextView.text = TimeHelper.formatToMMSS(item.duration.toInt())
+            youtubeImageView.setOnClickListener {
+                onYouTubeClicked(item)
+            }
         }
 
         override fun onClick(v: View?) {
         }
+
+        private fun onYouTubeClicked(item: Track) {
+            onYouTubeClickListener.get()?.openYouTube(item)
+        }
+    }
+
+    interface OnYouTubeClicked {
+        fun openYouTube(track: Track)
     }
 }
