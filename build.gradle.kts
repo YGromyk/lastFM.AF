34ce0ebc77bcf80757504a6b22cfd3d75c0e50ad
Yurii Gromyk <yurii.gromyk@appsfactory.de>
2019-09-03 09:10:26 +0200

    Update build tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
 
     }
     dependencies {
-        extra["kotlinVersion"] = "1.3.40"
-        classpath("com.android.tools.build:gradle:3.4.1")
+        extra["kotlinVersion"] = "1.3.50"
+        classpath("com.android.tools.build:gradle:3.5.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlinVersion"]}")
         classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0")
         // NOTE: Do not place your application dependencies here; they belong
@@ -27,9 +27,4 @@ tasks {
     registering(Delete::class) {
         delete(buildDir)
     }
-}
-
-
-object LibrariesVersions {
-    const val kotlin = "1.3.40"
 }
